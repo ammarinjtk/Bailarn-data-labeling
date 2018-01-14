46fb7f127f49549dd91d6f64ccd5e28cd9e4b2b9
player <ammarinjtk@outlook.com>
2018-01-14 22:45:33 +0700

    Update shows page for input form

--- app.js
+++ app.js
@@ -18,10 +18,14 @@ var pantipSchema = new mongoose.Schema({
   topic: String,
   title: String,
   url: String,
-  problem: Boolean,
-  keyword: Array,
-  sentiment: Number,
-  updated_date: Date,
+  is_problem: Boolean,
+  type: String,
+  type_keywords: Array,
+  company_name: Array,
+  company_keywords: Array,
+  sentiment: String,
+  is_updated: Boolean,
+  is_reported: Boolean
 });
 pantipSchema.plugin(mongoosePaginate);
 
@@ -57,11 +61,19 @@ app.get("/", function (req, res) {
   res.render("index");
 });
 
-app.get("/posts/page/:pageId", function (req, res) {
+app.get("/posts/:filter/page/:pageId", function (req, res) {
+
+  var totalPosts = 37076;
+
   var limitPerPage = 100;
-  Model.paginate({}, { page: req.params.pageId, limit: limitPerPage }, function (err, results) {
+  find_dict = {
+    "todo": { is_reported: false, is_updated: false },
+    "done": { is_reported: false, is_updated: true },
+    "report": { is_reported: true },
+  };
+  Model.paginate(find_dict[req.params.filter], { page: req.params.pageId, limit: limitPerPage }, function (err, results) {
     // console.log(results.total);
-    res.render("posts", { pantipPosts: results.docs, limitPerPage: limitPerPage, currentPage: results.page, totalPage: Math.ceil(results.total / limitPerPage) });
+    res.render("posts", { pantipPosts: results.docs, limitPerPage: limitPerPage, currentPage: results.page, totalPage: Math.ceil(results.total / limitPerPage), postNum: results.total, totalNum: totalPosts, filter: req.params.filter });
   });
 });
 
@@ -76,23 +88,60 @@ app.get("/posts/page/:pageId/:id", function (req, res) {
     if (err) {
       console.log(err);
     } else {
-      res.render("shows", { id: id, pageId: pageId, pantipPost: result });
+      var topic_list = [];
+      var i = 0;
+      (result.topic.split(" ")).forEach(element => {
+        i++;
+        topic_list.push(" | <font color=#808080>[" + i + "]</font> " + element);
+      });
+      res.render("shows", { id: id, pageId: pageId, pantipPost: result, pantipTopic: topic_list.join("") });
     }
   });
   // res.render("shows", { id: id, pageId: pageId, data: data });
+});
+
+app.post("/posts/page/:pageId/:id/report", function (req, res) {
+  var id = req.params.id;
+  var pageId = req.params.pageId;
+
+  var reported = {
+    is_updated: true,
+    is_reported: true
+  }
+
+  // Model.findByIdAndUpdate(id, { $set: reported }, { new: true }, function (err, result) {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log("id: " + id + " Report successfully");
+  //     // console.log(result);
+  //   }
+  // });
+
+  res.render("report", { id: id, pageId: pageId, reported: reported })
+
 });
 
 app.put("/posts/page/:pageId/:id", function (req, res) {
   var id = req.params.id;
   var pageId = req.params.pageId;
 
-  res.send(req.body)
-  // var updated = {
-  //   problem: (req.body.problemFlag == 'yes' ? true : false),
-  //   sentiment: req.body.sentiment,
-  //   keyword: req.body.keywords.split(","),
-  //   updated_date: new Date(),
-  // }
+  var company_keyword_list = [];
+  req.body.company_keyword.forEach(element => {
+    company_keyword_list.push(element.split(','));
+  });;
+
+  var updated = {
+    is_problem: (req.body.is_problem == 'Problem' ? true : false),
+    type: (req.body.type == 'Other' ? req.body.other_type : req.body.type),
+    type_keywords: req.body.type_keyword,
+    company_name: req.body.company_name.split(','),
+    company_keywords: company_keyword_list,
+    sentiment: req.body.sentiment,
+    is_updated: true,
+    is_reported: false
+  }
+
   // Model.findByIdAndUpdate(id, { $set: updated }, { new: true }, function (err, result) {
   //   if (err) {
   //     console.log(err);
@@ -101,7 +150,8 @@ app.put("/posts/page/:pageId/:id", function (req, res) {
   //     // console.log(result);
   //   }
   // });
-  // res.render("submit", { id: id, pageId: pageId })
+
+  res.render("submit", { id: id, pageId: pageId, updated: updated })
 
 });
 
